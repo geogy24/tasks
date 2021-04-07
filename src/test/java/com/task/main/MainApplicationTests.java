de5a47eba6b04e27c586e9d9f22c9c9e24e17c03
Jorge Díaz <jorge.diaz@endava.com>
2021-04-07 10:42:01 -0500

    Main class test

--- src/test/java/com/task/main/MainApplicationTests.java
+++ src/test/java/com/task/main/MainApplicationTests.java
@@ -1,13 +1,6 @@
 package com.task.main;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MainApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+class MainApplicationTests {}
