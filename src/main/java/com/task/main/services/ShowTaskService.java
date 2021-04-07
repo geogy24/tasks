3a3e0b1abae866258c0c79039f11e6dd476413b0
Jorge Díaz <jorge.diaz@endava.com>
2021-04-07 12:35:21 -0500

    Show task flow

---
+++ src/main/java/com/task/main/services/ShowTaskService.java
@@ -0,0 +1,26 @@
+package com.task.main.services;
+
+import com.task.main.exceptions.TaskNotFoundException;
+import com.task.main.models.Task;
+import com.task.main.repositories.TaskRepository;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ShowTaskService implements ShowTaskServiceInterface {
+    private final TaskRepository taskRepository;
+
+    public ShowTaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    @SneakyThrows
+    public Task execute(Long id) {
+        log.info("Show task by id {}", id);
+        return this.taskRepository.findById(id)
+                .orElseThrow(TaskNotFoundException::new);
+    }
+}
---
+++ src/main/java/com/task/main/services/ShowTaskServiceInterface.java
@@ -0,0 +1,7 @@
+package com.task.main.services;
+
+import com.task.main.models.Task;
+
+public interface ShowTaskServiceInterface {
+    Task execute(Long id);
+}
